377590b6081cb1f415ab3976c08d37cd03f0b0f2
Rochin <emirochi22@gmail.com>
2023-11-14 23:26:48 -0700

    second commit

--- Form4.Designer.cs
+++ Form4.Designer.cs
@@ -36,6 +36,24 @@
             changeThemeToolStripMenuItem = new ToolStripMenuItem();
             darToolStripMenuItem = new ToolStripMenuItem();
             toolStripMenuItem2 = new ToolStripMenuItem();
+            button2 = new Button();
+            button1 = new Button();
+            textBox7 = new TextBox();
+            textBox6 = new TextBox();
+            textBox5 = new TextBox();
+            textBox4 = new TextBox();
+            textBox3 = new TextBox();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            label9 = new Label();
+            label8 = new Label();
+            label7 = new Label();
+            label6 = new Label();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
             toolStrip1.SuspendLayout();
             SuspendLayout();
             // 
@@ -46,7 +64,7 @@
             toolStrip1.Location = new Point(0, 0);
             toolStrip1.Name = "toolStrip1";
             toolStrip1.Padding = new Padding(0, 0, 3, 0);
-            toolStrip1.Size = new Size(1746, 34);
+            toolStrip1.Size = new Size(1752, 34);
             toolStrip1.TabIndex = 4;
             toolStrip1.Text = "toolStrip1";
             // 
@@ -78,28 +96,198 @@
             // 
             changeThemeToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { darToolStripMenuItem, toolStripMenuItem2 });
             changeThemeToolStripMenuItem.Name = "changeThemeToolStripMenuItem";
-            changeThemeToolStripMenuItem.Size = new Size(270, 34);
+            changeThemeToolStripMenuItem.Size = new Size(232, 34);
             changeThemeToolStripMenuItem.Text = "&Change Theme";
             // 
             // darToolStripMenuItem
             // 
             darToolStripMenuItem.Name = "darToolStripMenuItem";
-            darToolStripMenuItem.Size = new Size(270, 34);
+            darToolStripMenuItem.Size = new Size(215, 34);
             darToolStripMenuItem.Text = "&Dark theme";
             darToolStripMenuItem.Click += darToolStripMenuItem_Click;
             // 
             // toolStripMenuItem2
             // 
             toolStripMenuItem2.Name = "toolStripMenuItem2";
-            toolStripMenuItem2.Size = new Size(270, 34);
+            toolStripMenuItem2.Size = new Size(215, 34);
             toolStripMenuItem2.Text = "&White theme";
             toolStripMenuItem2.Click += toolStripMenuItem2_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(967, 782);
+            button2.Name = "button2";
+            button2.Size = new Size(112, 34);
+            button2.TabIndex = 38;
+            button2.Text = "Ver Comics";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(967, 717);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 37;
+            button1.Text = "Add Comic";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(843, 633);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(376, 31);
+            textBox7.TabIndex = 36;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(844, 559);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(376, 31);
+            textBox6.TabIndex = 35;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(844, 484);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(376, 31);
+            textBox5.TabIndex = 34;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(844, 407);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(376, 31);
+            textBox4.TabIndex = 33;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(844, 332);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(376, 31);
+            textBox3.TabIndex = 32;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(844, 262);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(376, 31);
+            textBox2.TabIndex = 31;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(844, 192);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(376, 31);
+            textBox1.TabIndex = 30;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(778, 639);
+            label9.Name = "label9";
+            label9.Size = new Size(54, 25);
+            label9.TabIndex = 29;
+            label9.Text = "Artist";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(778, 565);
+            label8.Name = "label8";
+            label8.Size = new Size(60, 25);
+            label8.TabIndex = 28;
+            label8.Text = "Writer";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(789, 490);
+            label7.Name = "label7";
+            label7.Size = new Size(49, 25);
+            label7.TabIndex = 27;
+            label7.Text = "Type";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(726, 413);
+            label6.Name = "label6";
+            label6.Size = new Size(112, 25);
+            label6.TabIndex = 26;
+            label6.Text = "Date Release";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(730, 338);
+            label5.Name = "label5";
+            label5.Size = new Size(108, 25);
+            label5.TabIndex = 25;
+            label5.Text = "Date Added";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(791, 262);
+            label4.Name = "label4";
+            label4.Size = new Size(47, 25);
+            label4.TabIndex = 24;
+            label4.Text = "Tags";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(754, 198);
+            label3.Name = "label3";
+            label3.Size = new Size(84, 25);
+            label3.TabIndex = 23;
+            label3.Text = "Publisher";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI Semibold", 14F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            label2.Location = new Point(923, 115);
+            label2.Name = "label2";
+            label2.Size = new Size(156, 38);
+            label2.TabIndex = 22;
+            label2.Text = "Add Comic";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Semibold", 18F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            label1.Location = new Point(958, 33);
+            label1.Name = "label1";
+            label1.Size = new Size(80, 48);
+            label1.TabIndex = 21;
+            label1.Text = "Pile";
             // 
             // Form4
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1746, 763);
+            ClientSize = new Size(1752, 873);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox7);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
             Controls.Add(toolStrip1);
             Name = "Form4";
             Text = "Form4";
@@ -118,5 +306,23 @@
         private ToolStripMenuItem changeThemeToolStripMenuItem;
         private ToolStripMenuItem darToolStripMenuItem;
         private ToolStripMenuItem toolStripMenuItem2;
+        private Button button2;
+        private Button button1;
+        private TextBox textBox7;
+        private TextBox textBox6;
+        private TextBox textBox5;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Label label9;
+        private Label label8;
+        private Label label7;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label1;
     }
 }
